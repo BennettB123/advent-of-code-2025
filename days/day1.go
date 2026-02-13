91f2b18e723b7f221b2a08f942aa94f8d2a13d3a
BennettB123 <bennett.ballentine@gmail.com>
2026-02-12 22:07:22 -0500

    feat: create scaffolding for puzzles

---
+++ days/day1.go
@@ -0,0 +1,7 @@
+package days
+
+import "fmt"
+
+func Day1_main(input []byte) {
+	fmt.Println("Solving Day 1")
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/BennettB123/advent-of-code-2025/days"
+)
+
+var NumDays int = 1
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("2 arguments expected. Usage:\n" +
+			"  ./advent-of-code-2025 <day#> <inputFile>")
+		os.Exit(1)
+	}
+
+	day, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("Unable to parse day to int. Expected an integer but got: '%s'\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	fileContents, err := os.ReadFile(os.Args[2])
+	if err != nil {
+		fmt.Printf("Unable to read input file. Error: %s\n", err)
+		os.Exit(1)
+	}
+
+	switch day {
+	case 1:
+		days.Day1_main(fileContents)
+	default:
+		fmt.Printf("Invalid day. Only %d days are complete", NumDays)
+		os.Exit(1)
+	}
+}
