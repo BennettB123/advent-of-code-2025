928daac99433c2231c6cead1c9cfca786ca62e0a
BennettB123 <bennett.ballentine@gmail.com>
2026-02-13 22:42:16 -0500

    feat: day 1 star 2 complete!

--- days/day1.go
+++ days/day1.go
@@ -53,5 +53,43 @@ func starOne(input []byte) {
 }
 
 func starTwo(input []byte) {
-	fmt.Printf("Star 2 answer: todo \n")
+	lines := strings.Lines(string(input))
+
+	numTimesZero := 0
+	dialPos := 50
+
+	fmt.Printf("Dial starting at %d \n", dialPos)
+
+	for line := range lines {
+		line = strings.TrimSpace(line)
+
+		isLeft := line[0] == 'L'
+		clicks, err := strconv.Atoi(line[1:])
+		if err != nil {
+			fmt.Printf("invalid input. Expected integer but got '%s'\n", line[1:])
+			os.Exit(1)
+		}
+
+		// ugly brute force because I'm tired of trying to fix edge cases
+		for range clicks {
+			if isLeft {
+				dialPos -= 1
+			} else {
+				dialPos += 1
+			}
+
+			if dialPos < 0 {
+				dialPos += 100
+			} else if dialPos > 99 {
+				dialPos -= 100
+			}
+
+			if dialPos == 0 {
+				numTimesZero++
+			}
+		}
+
+	}
+
+	fmt.Printf("Star 2 answer: %d \n", numTimesZero)
 }
