78e3dd3f05b00820e0ca8fea42d4172666b99e8f
BennettB123 <bennett.ballentine@gmail.com>
2026-02-13 21:25:50 -0500

    feat: day 1 star 1 complete!

--- days/day1.go
+++ days/day1.go
@@ -1,7 +1,57 @@
 package days
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func Day1_main(input []byte) {
-	fmt.Println("Solving Day 1")
+	starOne(input)
+	starTwo(input)
+}
+
+func starOne(input []byte) {
+	lines := strings.Lines(string(input))
+
+	numTimesZero := 0
+	dialPos := 50
+
+	// fmt.Printf("Dial starting at %d \n", dialPos)
+
+	for line := range lines {
+		line = strings.TrimSpace(line)
+
+		isLeft := line[0] == 'L'
+		value, err := strconv.Atoi(line[1:])
+		if err != nil {
+			fmt.Printf("invalid input. Expected integer but got '%s'\n", line[1:])
+			os.Exit(1)
+		}
+
+		if isLeft {
+			dialPos -= value
+			for dialPos < 0 {
+				dialPos += 100
+			}
+		} else {
+			dialPos += value
+			for dialPos > 99 {
+				dialPos -= 100
+			}
+		}
+
+		// fmt.Printf("Rotating %s = %d \n", line, dialPos)
+
+		if dialPos == 0 {
+			numTimesZero++
+		}
+	}
+
+	fmt.Printf("Star 1 answer: %d \n", numTimesZero)
+}
+
+func starTwo(input []byte) {
+	fmt.Printf("Star 2 answer: todo \n")
 }
