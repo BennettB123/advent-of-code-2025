1cfdd5ffb5a5a76f81049b99c71f7453f31124b4
BennettB123 <bennett.ballentine@gmail.com>
2026-02-16 16:14:34 -0500

    feat: day 3 star 2 complete!

--- days/day3.go
+++ days/day3.go
@@ -2,6 +2,7 @@ package days
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
@@ -12,7 +13,7 @@ func Day3(input []byte) {
 
 func day3_star1(input []byte) {
 	// 2 pass strategy
-	//   1st pass finds the largest number
+	//   1st pass finds the largest number (can't be the final digit)
 	//   2nd pass finds the largest number that follows the first
 	accumulator := 0
 
@@ -23,7 +24,6 @@ func day3_star1(input []byte) {
 		largestNum := 0
 		largestNumIdx := 0
 
-		// first number cannot be the final digit in the line
 		for idx, digit := range line[:len(line)-1] {
 			num := int(digit - '0')
 			if num > largestNum {
@@ -49,5 +49,41 @@ func day3_star1(input []byte) {
 }
 
 func day3_star2(input []byte) {
-	fmt.Printf("Star 2 answer: todo \n")
+	// 12 pass strategy
+	//   1st pass finds the largest number (can't be within 12 digits from the end)
+	//   2nd pass finds the largest number that follows the first
+	//   3nd pass finds the largest number that follows the second
+	//   ...
+	var accumulator int64 = 0
+	numDigitsNeeded := 12
+
+	for line := range strings.Lines(string(input)) {
+		line = strings.TrimSpace(line)
+
+		currIdx := -1
+		var joltage int64 = 0
+
+		for i := range numDigitsNeeded {
+			largestNum := 0
+
+			for idx, digit := range line {
+				lastIdx := len(line) - numDigitsNeeded + i
+				if idx <= currIdx || idx > lastIdx {
+					continue
+				}
+
+				num := int(digit - '0')
+				if num > largestNum {
+					largestNum = num
+					currIdx = idx
+				}
+			}
+
+			joltage += int64(largestNum * int(math.Pow(10, float64(numDigitsNeeded-i-1))))
+		}
+
+		accumulator += joltage
+	}
+
+	fmt.Printf("Star 2 answer: %d \n", accumulator)
 }
